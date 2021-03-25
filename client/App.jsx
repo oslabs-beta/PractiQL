8f0664468979b02bef2c22e36f725ec32e490ba6
Anthony <tonegiovanni@gmail.com>
2021-03-25 11:45:13 -0400

    Adds component to display query results.

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from './components/Editor';
 
-export default function App() {
-  return <div>Hello World</div>;
+const resultsObject = {
+  persons: {
+    person1: {
+      name: 'john',
+      age: 23,
+    },
+    person2: {
+      name: 'mary',
+      age: 28,
+    },
+    person3: {
+      name: 'alex',
+      age: 32,
+    },
+  },
+};
+
+const resultsObjectB = {
+  persons: {
+    person1: {
+      name: 'daniel',
+      age: 23,
+    },
+  },
+};
+
+const resultsObjectC = {
+  persons: {
+    person1: {
+      name: 'mary',
+      age: 28,
+    },
+    person2: {
+      name: 'jackie',
+      age: 21,
+    },
+    person3: {
+      name: 'chris',
+      age: 38,
+    },
+    person4: {
+      name: 'charlie',
+      age: 4000,
+    },
+    person5: {
+      name: 'larry',
+      age: 220,
+    },
+    person6: {
+      name: 'mark',
+      age: 25,
+    },
+    person7: {
+      name: 'lee',
+      age: 28,
+    },
+    person8: {
+      name: 'peter',
+      age: 32,
+    },
+  },
+};
+
+function App() {
+  return (
+    <>
+      <div className="queryResults">
+        <Editor
+          language="javascript"
+          displayName="Query Results"
+          value={resultsObject}
+        />
+        <Editor
+          language="javascript"
+          displayName="Query Results"
+          value={resultsObjectB}
+        />
+        <Editor
+          language="javascript"
+          displayName="Query Results"
+          value={resultsObjectC}
+        />
+      </div>
+    </>
+  );
 }
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         },
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
