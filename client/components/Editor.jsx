c61a6e79c1a764240f4f6256dec0cb64aded8b33
Anthony <tonegiovanni@gmail.com>
2021-03-26 01:34:27 -0400

    Expands and collapses editor instances smoothly using CSS transition

--- client/components/Editor.jsx
+++ client/components/Editor.jsx
@@ -19,21 +19,26 @@ export default function Editor(props) {
 
   const handleAccordian = () => {
     console.log('Editor.jsx: handleAccordion');
-    console.log(accordion);
+    // Grabs full expanded height of editor instance
+    const sizer = accordion.divWrapper.querySelector('.CodeMirror-sizer').style
+      .minHeight;
     if (accordion.accordionValue === 'collapsed') {
-      accordion.divWrapper.style.maxHeight = '100%';
-      accordion.divWrapper.style.height = '100%';
+      accordion.divWrapper.style.height = sizer;
+      accordion.divWrapper.style.maxHeight = sizer;
       setAccordion((prev) => {
         return { ...prev, accordionValue: 'expanded', btnText: 'collapse' };
       });
     } else if (accordion.accordionValue === 'expanded') {
       accordion.divWrapper.style.maxHeight = '150px';
-      accordion.divWrapper.style.height = '100px';
+      accordion.divWrapper.style.height = '150px';
       setAccordion((prev) => {
         return { ...prev, accordionValue: 'collapsed', btnText: 'expand' };
       });
     }
-    accordion.editorToGrab.refresh();
+    // CSS transition runs asynchronously. refresh async so it's invoked after css transition animation
+    setTimeout(() => {
+      accordion.editorToGrab.refresh();
+    }, 100);
   };
 
   return (
