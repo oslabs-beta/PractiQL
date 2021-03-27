877e99785e44351d3c28ecf01ebbd5eb6bf93b45
Anthony <tonegiovanni@gmail.com>
2021-03-26 22:27:57 -0400

    Creates body.scss. Imports body.scss into styles.scss. Removes margin from body.

--- client/components/Output.jsx
+++ client/components/Output.jsx
@@ -67,7 +67,6 @@ export default function Output(props) {
         options={{
           mode: language,
           lineNumbers: true,
-          lineWrapping: true,
           readOnly: true,
           scrollbarStyle: 'overlay',
         }}
