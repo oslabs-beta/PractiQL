af42fd8c5ff985c5f8446864713023983838c0cc
Anthony <tonegiovanni@gmail.com>
2021-03-31 14:15:18 -0400

    folding code in CodeMirror with GraphQL language.

--- client/components/Output.jsx
+++ client/components/Output.jsx
@@ -2,50 +2,74 @@ import CodeMirror, { overlayMode } from 'codemirror';
 import React, { useState, useEffect, useCallback } from 'react';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-
 export default function Output(props) {
   const [editorToGrab, setEditor] = useState(null);
   const [value, setValue] = useState('');
-  const { displayName, language, results, onChange, theme, numOfQueries } = props;
-  
-  useEffect(()=> {
+  const {
+    displayName,
+    language,
+    results,
+    onChange,
+    theme,
+    numOfQueries,
+  } = props;
+
+  // {
+  //   continents{
+  //     name
+  //   }
+  // }
+
+  // {
+  //   languages{
+  //     name
+  //   }
+  // }
+
+  useEffect(() => {
     console.log('Output.jsx: useEffect invoked');
-    if(editorToGrab) {
-    console.log('Output.jsx: in if');
-      // Create a Codemirror headless that means in the bg, and not attached to the DOM
+    if (editorToGrab) {
+      console.log('Output.jsx: in if');
       let count = 1;
       let lastLine = 0;
-      for(let key in results){
+      console.log(results);
+      for (let key in results) {
         const instance = new CodeMirror(document.getElementById('hidden'), {
-          value: JSON.stringify(results[key], null, 2)
+          value: JSON.stringify(results[key], null, 2),
         });
-
-        if(count === 1){
-          editorToGrab.foldCode(1);
-        } else {
-          editorToGrab.foldCode(lastLine+1);
+        console.log(count);
+        if (count > 1) {
+          // editorToGrab.foldCode(1);
+          // editorToGrab.foldCode(25);
         }
+        console.log(key + ' lastLine: ' + instance.lastLine());
+        console.log(key + ' lineCount: ' + instance.lineCount());
         count++;
-        lastLine += instance.lineCount();
-        console.log(key + " lastline: " + lastLine);
       }
+      editorToGrab.foldCode(28);
+      editorToGrab.foldCode(26);
+      editorToGrab.foldCode(25);
+      editorToGrab.foldCode(24);
+      editorToGrab.foldCode(1);
+      //   for (let key in results) {
+      //     const instance = new CodeMirror(document.getElementById('hidden'), {
+      //       value: JSON.stringify(results[key], null, 2),
+      //     });
+      //     // create a CodeMirror instance not attached to the DOM
+      //     console.log(key + ' lastLine: ' + instance.lastLine());
+      //     console.log(key + ' lineCount: ' + instance.lineCount());
+      //     // if (count === 1) {
+      //     //   editorToGrab.foldCode(1);
+      //     // } else {
+      //     //   editorToGrab.foldCode(lastLine + 2);
+      //     // }
+      //     // count++;
+      //     // lastLine += instance.lineCount();
+      //     // console.log(key + ' lastline: ' + lastLine);
+      //   }
     }
-    console.log('Output.jsx: end of useEffect')
+    console.log('Output.jsx: end of useEffect');
   }, [results]);
-
-// through state
-  // value, setValue
-
-// custom fn
-  // grabbing editor value
-  // resetting value with same value but collapsed
-
-// didMount
-  // grab editor
-  // 
-// didUpdate
-
-  // invoke a custom function
 
   return (
     <>
@@ -58,10 +82,11 @@ export default function Output(props) {
         }}
         on
         options={{
-          mode: language,
+          mode: 'javascript',
           foldGutter: true,
           gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
           readOnly: true,
+          lineNumbers: true,
           theme: theme,
           scrollbarStyle: null,
         }}
